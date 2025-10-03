87650b13ac6d5c292da5c074d21d0050627777a8
Anthony <anthonypriceone@gmail.com>
2025-10-04 02:03:08 +0300

    Add package config and env

--- 3-struct/api/api.go
+++ 3-struct/api/api.go
@@ -1,1 +1,13 @@
 package api
+
+import "jsoncli/config"
+
+type Client struct {
+	APIKey string
+}
+
+func NewClient(cfg *config.Config) *Client {
+	return &Client{
+		APIKey: cfg.Key,
+	}
+}
---
+++ 3-struct/config/config.go
@@ -0,0 +1,17 @@
+package config
+
+import "os"
+
+type Config struct {
+	Key string
+}
+
+func NewConfig() *Config {
+	key := os.Getenv("KEY")
+	if key == "" {
+		panic("KEY environment variable not set")
+	}
+	return &Config{
+		Key: key,
+	}
+}
